2dde6f0a66ba06eaf96a0bd62761f505e50b39de
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-11 19:41:45 +0300

    fix3

--- client/src/components/AdminTab3/AdminTab3.tsx
+++ client/src/components/AdminTab3/AdminTab3.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
+const dbOrders = [
+  {
+    id: 1,
+    email: 'abiba@gamil.com',
+    name: 'Abiba',
+    surname: 'Abibova',
+    patrname: 'Abibovna',
+    birthdate: new Date('2023-07-08 10:51:49.612 +0300'),
+    phoneNumber: 89289289289,
+    password: 'Abibaword',
+    residency: 'Uzbek',
+    likesAnimals: true,
+  },
+  {
+    id: 12,
+    email: 'ab0ba@gamil.com',
+    name: 'Ab0ba',
+    surname: 'Ab0bova',
+    patrname: 'Ab0bovna',
+    birthdate: new Date('2023-07-08 10:51:49.612 +0300'),
+    phoneNumber: 89289289289,
+    password: 'Ab0baword',
+    residency: 'Uzbek',
+    likesAnimals: true,
+  },
+  {
+    id: 13,
+    email: '12345@gamil.com',
+    name: '12345',
+    surname: '12345va',
+    patrname: '12345vna',
+    birthdate: new Date('2023-07-08 10:51:49.612 +0300'),
+    phoneNumber: 89289289289,
+    password: '12345word',
+    residency: 'Uzbek',
+    likesAnimals: true,
+  },
+];
+
 const AdminTab3 = () => {
   return <div>AdminTab3</div>;
 };
