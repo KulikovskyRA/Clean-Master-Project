d5721bee2ca259d295fda629a551f19b3121778c
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-12 14:17:14 +0300

    Migrations, seeders

---
+++ server/db/seeders/20230712111051-Admins.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      'Admins',
+      [
+        {
+          email: 'adminboba@biba.ru',
+          password: 'adminbiba',
+          adminName: 'adminboba',
+          createdAt: Sequelize.DataTypes.NOW().toString(),
+          updatedAt: Sequelize.DataTypes.NOW().toString(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Admins', null, {});
+  },
+};
