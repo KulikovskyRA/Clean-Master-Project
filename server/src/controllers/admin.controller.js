81643f4e931e6d163a78184bc273e08eabe90241
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-12 14:49:33 +0300

    authAdminBack

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -19,33 +19,12 @@ module.exports.adminLogin = async (req, res) => {
           auth: true,
         });
       } else {
+        res.sendStatus(403);
       }
+    } else {
+      res.sendStatus(403);
     }
-  } catch (error) {}
+  } catch (error) {
+    res.status(404).json({ error: 'Ошибка авторизации.' });
+  }
 };
-
-// router.post('/login', async (req, res) => {
-//   const { username, password } = req.body;
-
-//   try {
-//     const check = await User.findOne({ where: { username }, raw: true });
-//     if (check) {
-//       const hashPass = await bcrypt.compare(password, check.password);
-
-//       if (hashPass) {
-//         req.session.user = check;
-
-//         res
-//           .status(200)
-//           .json({ id: check.id, username: check.username, auth: true });
-//       } else {
-//         res.sendStatus(403);
-//       }
-//     } else {
-//       res.sendStatus(403);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(404).json({ error: 'Ошибка авторизации.' });
-//   }
-// });
