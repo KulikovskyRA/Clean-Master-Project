dbbe74570a34205038cdc8117e6d75ee09014f73
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-12 18:27:12 +0300

    reduxAuthAdmin

--- client/src/redux/authSlice.tsx
+++ client/src/redux/authSlice.tsx
@@ -4,6 +4,7 @@ const initialState = {
   type: '',
   id: 0,
   name: '',
+  email: '',
 };
 
 const authSlice = createSlice({
@@ -14,10 +15,11 @@ const authSlice = createSlice({
       state.type = action.payload.type;
       state.name = action.payload.name;
       state.id = action.payload.id;
+      state.email = action.payload.email;
     },
   },
 });
 
 export default authSlice.reducer;
 
-export const { adminAuth } = authSlice.actions;
+export const { authReducer } = authSlice.actions;
