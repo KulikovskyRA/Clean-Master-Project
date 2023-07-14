c87c41e7d3ee68a0f88e94c7f10feeaf62a9fedc
Artem <4453735@gmail.com>
2023-07-14 13:45:09 +0400

    Add edit profile form

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,11 +1,21 @@
 const { User } = require('../../db/models');
 
 module.exports.edit = async (req, res) => {
-  const { userName, phoneNumber, email } = req.body;
-  const id = req.params;
-  const updateUser = await User.update({ lastName: "Doe" }, {
-    where: {
-      lastName: null
-    }
-  });
+  const { userName, phoneNumber, email } = req.body.values;
+  //const id = req.params;
+  try {
+    const updateUser = await User.update({ userName, phoneNumber, email }, {
+      where: {
+        id: req.body.id
+      }
+    });
+    const sessionUser = { email, name: userName, id: req.body.id, phoneNumber };
+    req.session.user = sessionUser;
+    res
+      .status(200)
+      .json({ user: sessionUser });
+    console.log(updateUser);
+  } catch (error) {
+    console.log(error);
+  }
 };
