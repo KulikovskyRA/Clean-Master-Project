04e6a50ee477ee7608e2d0efa9f117b583131e95
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-14 15:11:29 +0300

    fix

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -4,17 +4,18 @@ module.exports.edit = async (req, res) => {
   const { userName, phoneNumber, email } = req.body.values;
   //const id = req.params;
   try {
-    const updateUser = await User.update({ userName, phoneNumber, email }, {
-      where: {
-        id: req.body.id
+    const updateUser = await User.update(
+      { userName, phoneNumber, email },
+      {
+        where: {
+          id: req.body.id,
+        },
       }
-    });
+    );
     const sessionUser = { email, name: userName, id: req.body.id, phoneNumber };
     req.session.user = sessionUser;
-    res
-      .status(200)
-      .json({ user: sessionUser });
-    console.log(updateUser);
+    res.status(200).json({ user: sessionUser });
+    // console.log(updateUser);
   } catch (error) {
     console.log(error);
   }
