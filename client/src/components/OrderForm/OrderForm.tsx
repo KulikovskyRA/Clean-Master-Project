5554bd8cea889dff954b15effd12a45ab42133b3
Artem <4453735@gmail.com>
2023-07-18 13:40:24 +0400

    Add rooms and baths inputs

--- client/src/components/OrderForm/OrderForm.tsx
+++ client/src/components/OrderForm/OrderForm.tsx
@@ -1,18 +1,73 @@
 import * as React from 'react';
 import { useState } from "react";
+import { Button, Checkbox, Form, Input } from 'antd';
 
 const OrderForm = () => {
-  const [ roomsCount, setRoomsCount ] = useState(0);
+  // const [ roomsCount, setRoomsCount ] = useState(1);
+  // const [ bathCount, setBathCount ] = useState(1);
 
-  const incrementRooms = () => {
-    setRoomsCount(roomsCount + 1);
+  const [ formData, setFormData ] = useState({
+    rooms: 1,
+    baths: 1,
+    // Add more form fields here
+  });
+
+  const handleIncrement = (fieldName) => {
+    switch (fieldName) {
+      case 'roomsCount':
+        setFormData((prev) => ({ ...prev, rooms: prev.rooms + 1 }));
+        // setRoomsCount(roomsCount + 1);
+        break;
+      case 'bathCount':
+        setFormData((prev) => ({ ...prev, baths: prev.baths + 1 }));
+        //setBathCount(bathCount + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleDecrement = (fieldName) => {
+    switch (fieldName) {
+      case 'roomsCount':
+        setFormData((prev) => ({ ...prev, rooms: prev.rooms - 1 }));
+        if (formData.rooms <= 1) {
+          setFormData((prev) => ({ ...prev, rooms: 1 }));
+        }
+        break;
+      case 'bathCount':
+        setFormData((prev) => ({ ...prev, baths: prev.baths - 1 }));
+        if (formData.baths <= 1) {
+          setFormData((prev) => ({ ...prev, baths: 1 }));
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form values:', formData);
   };
   return (
 
-    <form>
-      <button>-</button>
-      <input type="text" value={roomsCount} readOnly/>
-      <div onClick={incrementRooms}>+</div>
+    <form onSubmit={handleSubmit}>
+      <label>Количество комнат</label>
+      <div onClick={() => handleDecrement('roomsCount')}>-</div>
+      <input name="rooms" type="text" value={formData.rooms} onChange={handleChange}/>
+      <div onClick={() => handleIncrement('roomsCount')}>+</div>
+
+      <label>Количество санузлов</label>
+      <div onClick={() => handleDecrement('bathCount')}>-</div>
+      <input name="baths" type="text" value={formData.baths} onChange={handleChange}/>
+      <div onClick={() => handleIncrement('bathCount')}>+</div>
+
+      <button type="submit">Отправить</button>
     </form>
 
   );
