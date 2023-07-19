d62da66366c4a384d89b36d35018e68374315869
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-19 18:43:32 +0300

    Уменьшил размер формы, изменил кнопки, отступы

--- client/src/components/OrderForm/OrderForm.tsx
+++ client/src/components/OrderForm/OrderForm.tsx
@@ -22,7 +22,6 @@ const { VITE_URL } = import.meta.env;
 
 import { useSelector } from 'react-redux';
 
-// console.log(futureDates);
 const OrderForm = () => {
   const navigate = useNavigate();
 
@@ -146,174 +145,187 @@ const OrderForm = () => {
         },
       }}
     >
-      {step === 0 && (
-        <>
-          {services
-            .filter((el) => el.default === true)
-            .map((el) => (
-              <div key={`default${el.id}`}>
-                <Text>{el.title}</Text>
-                <Button.Group>
-                  {formServices[el.id] > 0 ? (
-                    <Button onClick={() => handleDecrement(el)}>
-                      <MinusOutlined />
-                    </Button>
+      <div style={{ width: 450 }}>
+        {step === 0 && (
+          <>
+            {services
+              .filter((el) => el.default === true)
+              .map((el) => (
+                <div key={`default${el.id}`}>
+                  <Text>{el.title}</Text>
+                  <Row style={{ marginBottom: 10, marginTop: 3 }}>
+                    <Button.Group>
+                      {formServices[el.id] > 0 ? (
+                        <Button onClick={() => handleDecrement(el)}>
+                          <MinusOutlined />
+                        </Button>
+                      ) : (
+                        <Button disabled>
+                          <MinusOutlined />
+                        </Button>
+                      )}
+                      <Button>{formServices[el.id]}</Button>
+                      <Button onClick={() => handleIncrement(el)}>
+                        <PlusOutlined />
+                      </Button>
+                    </Button.Group>
+                  </Row>
+                </div>
+              ))}
+
+            <Text>Когда к вам приехать?</Text>
+            <Row style={{ marginBottom: 10, marginTop: 3 }}>
+              <select name="date" onChange={handleChange}>
+                {futureDates.map((el, i) => {
+                  return (
+                    <option key={i} value={el}>
+                      {el.toLocaleString('ru', {
+                        day: 'numeric',
+                        month: 'long',
+                        weekday: 'long',
+                      })}
+                    </option>
+                  );
+                })}
+              </select>
+              <select name="time" onChange={handleChange}>
+                {futureTimes.map((el, i) => (
+                  <option value={el} key={i}>
+                    {el}
+                  </option>
+                ))}
+              </select>
+            </Row>
+            <Button.Group>
+              <Button disabled>НАЗАД</Button>
+              <Button onClick={handleNextStep}>ДАЛЕЕ</Button>
+            </Button.Group>
+          </>
+        )}
+
+        {step === 1 && (
+          <>
+            {services
+              .filter((el) => el.default === false && el.single === false)
+              .map((el) => (
+                <div key={`not-single${el.id}`}>
+                  <Text>{el.title}</Text>
+                  <Row style={{ marginBottom: 10, marginTop: 3 }}>
+                    <Button.Group>
+                      {formServices[el.id] > 0 ? (
+                        <Button onClick={() => handleDecrement(el)}>
+                          <MinusOutlined />
+                        </Button>
+                      ) : (
+                        <Button disabled>
+                          <MinusOutlined />
+                        </Button>
+                      )}
+                      <Button>{formServices[el.id]}</Button>
+
+                      <Button onClick={() => handleIncrement(el)}>
+                        <PlusOutlined />
+                      </Button>
+                    </Button.Group>
+                  </Row>
+                </div>
+              ))}
+
+            {services
+              .filter((el) => el.single === true)
+              .map((el) => (
+                <div key={`ingle${el.id}`}>
+                  {formServices[el.id] ? (
+                    <Checkbox.Group>
+                      <Checkbox
+                        checked
+                        style={{ lineHeight: '32px' }}
+                        onChange={() => handleChangeCheckBox(el)}
+                      >
+                        {el.title}
+                      </Checkbox>
+                    </Checkbox.Group>
                   ) : (
-                    <Button disabled>
-                      <MinusOutlined />
-                    </Button>
+                    <Checkbox.Group>
+                      <Checkbox
+                        style={{ lineHeight: '32px' }}
+                        onChange={() => handleChangeCheckBox(el)}
+                      >
+                        {el.title}
+                      </Checkbox>
+                    </Checkbox.Group>
                   )}
-                  <Button>{formServices[el.id]}</Button>
-                  <Button onClick={() => handleIncrement(el)}>
-                    <PlusOutlined />
-                  </Button>
-                </Button.Group>
-              </div>
-            ))}
+                </div>
+              ))}
 
-          <Text>Когда к вам приехать?</Text>
-
-          <select name="date" onChange={handleChange}>
-            {futureDates.map((el, i) => {
-              return (
-                <option key={i} value={el}>
-                  {el.toLocaleString('ru', {
-                    day: 'numeric',
-                    month: 'long',
-                    weekday: 'long',
-                  })}
-                </option>
-              );
-            })}
-          </select>
-          <select name="time" onChange={handleChange}>
-            {futureTimes.map((el, i) => (
-              <option value={el} key={i}>
-                {el}
-              </option>
-            ))}
-          </select>
-          <Button.Group>
-            <Button disabled>НАЗАД</Button>
-            <Button onClick={handleNextStep}>ДАЛЕЕ</Button>
-          </Button.Group>
-        </>
-      )}
-
-      {step === 1 && (
-        <>
-          {services
-            .filter((el) => el.default === false && el.single === false)
-            .map((el) => (
-              <div key={`not-single${el.id}`}>
-                <Text>{el.title}</Text>
-                <Button.Group>
-                  {formServices[el.id] > 0 ? (
-                    <Button onClick={() => handleDecrement(el)}>
-                      <MinusOutlined />
-                    </Button>
-                  ) : (
-                    <Button disabled>
-                      <MinusOutlined />
-                    </Button>
-                  )}
-                  <Button>{formServices[el.id]}</Button>
-
-                  <Button onClick={() => handleIncrement(el)}>
-                    <PlusOutlined />
-                  </Button>
-                </Button.Group>
-              </div>
-            ))}
-
-          {services
-            .filter((el) => el.single === true)
-            .map((el) => (
-              <div key={`ingle${el.id}`}>
-                {formServices[el.id] ? (
-                  <Checkbox.Group>
-                    <Checkbox
-                      checked
-                      style={{ lineHeight: '32px' }}
-                      onChange={() => handleChangeCheckBox(el)}
-                    >
-                      {el.title}
-                    </Checkbox>
-                  </Checkbox.Group>
-                ) : (
-                  <Checkbox.Group>
-                    <Checkbox
-                      style={{ lineHeight: '32px' }}
-                      onChange={() => handleChangeCheckBox(el)}
-                    >
-                      {el.title}
-                    </Checkbox>
-                  </Checkbox.Group>
-                )}
-              </div>
-            ))}
-
-          <Input
-            onChange={handleChange}
-            type="text"
-            name="info"
-            placeholder="Добавьте комментарий"
-            value={formData.info}
-          />
-          <Button.Group>
-            <Button onClick={handlePrevStep}>НАЗАД</Button>
-            <Button onClick={handleNextStep}>ДАЛЕЕ</Button>
-          </Button.Group>
-        </>
-      )}
-      {step === 2 && (
-        <>
-          {user.id === 0 && (
-            <>
-              <Input
-                onChange={handleChange}
-                type="text"
-                name="phoneNumber"
-                placeholder="Введите номер телефона"
-                value={formData.phoneNumber}
-              />
-              <Input
-                onChange={handleChange}
-                type="email"
-                name="email"
-                placeholder="Введите email"
-                value={formData.email}
-              />
-            </>
-          )}
-          <Input
-            onChange={handleChange}
-            type="text"
-            name="city"
-            value={formData.city}
-            placeholder="Введите город"
-          />
-          <Input
-            onChange={handleChange}
-            type="text"
-            name="street"
-            value={formData.street}
-            placeholder="Введите улицу и дом"
-          />
-          <Input
-            onChange={handleChange}
-            type="text"
-            name="flat"
-            value={formData.flat}
-            placeholder="Введите номер квартиры"
-          />
-          <Button.Group>
-            <Button onClick={handlePrevStep}>НАЗАД</Button>
-            <Button onClick={handleSubmit}>Оформить заказ</Button>
-          </Button.Group>
-        </>
-      )}
-      <Title>{total}</Title>
+            <Input
+              style={{ marginBottom: 10, marginTop: 3 }}
+              onChange={handleChange}
+              type="text"
+              name="info"
+              placeholder="Добавьте комментарий"
+              value={formData.info}
+            />
+            <Button.Group>
+              <Button onClick={handlePrevStep}>НАЗАД</Button>
+              <Button onClick={handleNextStep}>ДАЛЕЕ</Button>
+            </Button.Group>
+          </>
+        )}
+        {step === 2 && (
+          <>
+            {user.id === 0 && (
+              <>
+                <Input
+                  style={{ marginBottom: 10, marginTop: 10 }}
+                  onChange={handleChange}
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Введите номер телефона"
+                  value={formData.phoneNumber}
+                />
+                <Input
+                  style={{ marginBottom: 15, marginTop: 3 }}
+                  onChange={handleChange}
+                  type="email"
+                  name="email"
+                  placeholder="Введите email"
+                  value={formData.email}
+                />
+              </>
+            )}
+            <Input
+              style={{ marginBottom: 5, marginTop: 10 }}
+              onChange={handleChange}
+              type="text"
+              name="city"
+              value={formData.city}
+              placeholder="Введите город"
+            />
+            <Input
+              style={{ marginBottom: 5, marginTop: 3 }}
+              onChange={handleChange}
+              type="text"
+              name="street"
+              value={formData.street}
+              placeholder="Введите улицу и дом"
+            />
+            <Input
+              style={{ marginBottom: 10, marginTop: 3 }}
+              onChange={handleChange}
+              type="text"
+              name="flat"
+              value={formData.flat}
+              placeholder="Введите номер квартиры"
+            />
+            <Button.Group>
+              <Button onClick={handlePrevStep}>НАЗАД</Button>
+              <Button onClick={handleSubmit}>Оформить заказ</Button>
+            </Button.Group>
+          </>
+        )}
+        <Title>{total}</Title>
+      </div>
     </ConfigProvider>
   );
 };
