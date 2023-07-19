bf7302ba13cd04aa630fc5c250b4204eca25b322
Artem <4453735@gmail.com>
2023-07-19 12:01:01 +0400

    Add new default services inputs

--- client/src/components/OrderForm/OrderForm.tsx
+++ client/src/components/OrderForm/OrderForm.tsx
@@ -11,82 +11,96 @@ const OrderForm = () => {
   // const [ roomsCount, setRoomsCount ] = useState(1);
   // const [ bathCount, setBathCount ] = useState(1);
   const [ step, setStep ] = useState(0);
-  const [ singleServices, setSingleServices ] = useState([]);
-  const [ noSingleServices, setNoSingleServices ] = useState([]);
+  //const [ singleServices, setSingleServices ] = useState([]);
+  //const [ noSingleServices, setNoSingleServices ] = useState([]);
+  const [ isChecked, setIsChecked ] = useState(false);
+  const [ services, setServices ] = useState([]);
+  const [ formServices, setFormServices ] = useState({});
 
   const [ formData, setFormData ] = useState({
     rooms: 1,
     baths: 1,
     date: 0,
-    time: '8:00',
+    time: '8:00'
     // Add more form fields here
   });
 
   useEffect(() => {
-    const singleServicesData = async () => {
+    const getAllServices = async () => {
       try {
-        const response = await fetch(`${VITE_URL}service/singleprice`);
+        const response = await fetch(`${VITE_URL}service/all`);
         const jsonData = await response.json();
-        setSingleServices(jsonData);
+        console.log(jsonData);
+        setServices(jsonData);
+        const initialServices = {};
+        jsonData.forEach(el => {
+          initialServices[el.id] = 0;
+        });
+
+        console.log(initialServices);
+        setFormServices(initialServices);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
-    const noSingleServicesData = async () => {
-      try {
-        const response = await fetch(`${VITE_URL}service/nosingleprice`);
-        const jsonData = await response.json();
-        setNoSingleServices(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    singleServicesData();
-    noSingleServicesData();
+    // const singleServicesData = async () => {
+    //   try {
+    //     const response = await fetch(`${VITE_URL}service/singleprice`);
+    //     const jsonData = await response.json();
+    //     setSingleServices(jsonData);
+    //   } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //   }
+    // };
+    //
+    // const noSingleServicesData = async () => {
+    //   try {
+    //     const response = await fetch(`${VITE_URL}service/nosingleprice`);
+    //     const jsonData = await response.json();
+    //     setNoSingleServices(jsonData);
+    //   } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //   }
+    // };
+    //
+    // singleServicesData();
+    // noSingleServicesData();
+    getAllServices();
   }, []);
 
-  const handleIncrement = (fieldName) => {
-    switch (fieldName) {
-      case 'roomsCount':
-        setFormData((prev) => ({ ...prev, rooms: prev.rooms + 1 }));
-        // setRoomsCount(roomsCount + 1);
-        break;
-      case 'bathCount':
-        setFormData((prev) => ({ ...prev, baths: prev.baths + 1 }));
-        //setBathCount(bathCount + 1);
-        break;
-      case 'services':
-        setFormData((prev) => ({ ...prev, services: {} }));
-        //setBathCount(bathCount + 1);
-        break;
-      default:
-        break;
-    }
+  // const handleIncrement = (e) => {
+  //   setFormServices((prev) => {...prev, [e.target.id]: prev[e.target.id] + 1})
+  //   }
+  // };
+
+  const handleIncrement = (e) => {
+    setFormServices((prev) => ({ ...prev, [e.target.id]: prev[e.target.id] + 1 }));
+    console.log(formServices);
   };
 
-  const handleDecrement = (fieldName) => {
-    switch (fieldName) {
-      case 'roomsCount':
-        setFormData((prev) => ({ ...prev, rooms: prev.rooms - 1 }));
-        if (formData.rooms <= 1) {
-          setFormData((prev) => ({ ...prev, rooms: 1 }));
-        }
-        break;
-      case 'bathCount':
-        setFormData((prev) => ({ ...prev, baths: prev.baths - 1 }));
-        if (formData.baths <= 1) {
-          setFormData((prev) => ({ ...prev, baths: 1 }));
-        }
-        break;
-      default:
-        break;
-    }
+
+  const handleDecrement = (e) => {
+    setFormServices((prev) => ({ ...prev, [e.target.id]: prev[e.target.id] - 1 }));
+    console.log(formServices);
   };
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleChangeInput = (e) => {
+    setFormServices({ ...formServices, [e.target.name]: e.target.value });
+    console.log(formServices);
+  };
+
+  const handleServicesChange = (e) => {
+    setFormData({ ...formData, services: { [e.target.name]: e.target.value } });
+  };
+
+  const handleCheckboxChange = (e) => {
+    setFormData({ ...formData, services: { [e.target.name]: e.target.checked } });
+    setIsChecked(true);
   };
 
   const handleNextStep = () => {
@@ -96,7 +110,21 @@ const OrderForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Form values:', formData);
+
   };
+
+  const handlePlus = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: (Number(prev[e.target.id]) + Number(e.target.value)) ?? 1
+    }));
+  };
+
+  // const handlePlus = (e) => {
+  //   setFormData((prev) => { ...prev, [e.target.id]: (prev[e.target.id]) + e.target.value });
+  //   //setOrders((prev) => prev.filter((el) => el.id !== orderId));
+  //   //setFormData({ ...formData, [e.target.id]: formData.name + e.target.value });
+  // };
 
   // const getSingleServices = async () => {
   //   const response = await fetch(`${VITE_URL}services/singlePrice`);
@@ -113,29 +141,41 @@ const OrderForm = () => {
 
     <form onSubmit={handleSubmit}>
       {(step === 0) && <>
-        <label>Количество комнат</label>
-        <div onClick={() => handleDecrement('roomsCount')}>-</div>
-        <input name="rooms" type="text" value={formData.rooms} onChange={handleChange}/>
-        <div onClick={() => handleIncrement('roomsCount')}>+</div>
 
-        <label>Количество санузлов</label>
-        <div onClick={() => handleDecrement('bathCount')}>-</div>
-        <input name="baths" type="text" value={formData.baths} onChange={handleChange}/>
-        <div onClick={() => handleIncrement('bathCount')}>+</div>
+        {services.filter((el) => el.default === true)
+          .map(el => (
+            <>
+              <label>{el.title}</label>
+              {(formServices[el.id] > 0) && <div id={el.id} onClick={handleDecrement}>-</div>}
+              <input name={el.id} type="text" value={formServices[el.id]} onChange={handleChangeInput}/>
+              <div id={el.id} onClick={handleIncrement}>+</div>
+            </>
+          ))
+        }
 
-        <label>Когда к вам приехать?</label>
-        <select name="date" onChange={handleChange}>
-          {futureDates.map((el, i) => {
-            return (<option key={i} value={el}>{el.toLocaleString("ru", {
-              day: "numeric",
-              month: "long",
-              weekday: "long",
-            })}</option>);
-          })}
-        </select>
-        <select name="time" onChange={handleChange}>
-          {futureTimes.map((el, i) => <option value={el} key={i}>{el}</option>)}
-        </select>
+        {/*<label>Количество комнат</label>*/}
+        {/*<div onClick={() => handleDecrement('roomsCount')}>-</div>*/}
+        {/*<input name="rooms" type="text" value={formData.rooms} onChange={handleChange}/>*/}
+        {/*<div onClick={() => handleIncrement('roomsCount')}>+</div>*/}
+
+        {/*<label>Количество санузлов</label>*/}
+        {/*<div onClick={() => handleDecrement('bathCount')}>-</div>*/}
+        {/*<input name="baths" type="text" value={formData.baths} onChange={handleChange}/>*/}
+        {/*<div onClick={() => handleIncrement('bathCount')}>+</div>*/}
+
+        {/*<label>Когда к вам приехать?</label>*/}
+        {/*<select name="date" onChange={handleChange}>*/}
+        {/*  {futureDates.map((el, i) => {*/}
+        {/*    return (<option key={i} value={el}>{el.toLocaleString("ru", {*/}
+        {/*      day: "numeric",*/}
+        {/*      month: "long",*/}
+        {/*      weekday: "long",*/}
+        {/*    })}</option>);*/}
+        {/*  })}*/}
+        {/*</select>*/}
+        {/*<select name="time" onChange={handleChange}>*/}
+        {/*  {futureTimes.map((el, i) => <option value={el} key={i}>{el}</option>)}*/}
+        {/*</select>*/}
       </>}
 
       {(step === 1) &&
@@ -144,8 +184,9 @@ const OrderForm = () => {
             return (<label>
               <input
                 type="checkbox"
-                checked={true}
-                // onChange={handleCheckboxChange}
+                name={el.id}
+                checked={isChecked}
+                onChange={handleCheckboxChange}
               />
               {el.title}
             </label>);
@@ -156,8 +197,8 @@ const OrderForm = () => {
               <>
                 <label>{el.title}</label>
                 <div onClick={() => handleDecrement('services')}>-</div>
-                <input name={el.id} type="text" value={formData.baths} onChange={handleChange}/>
-                <div onClick={() => handleIncrement('services')}>+</div>
+                <input name={el.id} type="text" value={formData.services[el.id]} onChange={handleServicesChange}/>
+                <div id={el.id} value={1} onClick={handlePlus}>+</div>
               </>);
           })}
 
