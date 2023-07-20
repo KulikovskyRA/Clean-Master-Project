e80ab5a0f50acb91efba879239987f64bc4eef7d
Lera Buzilova <buzilovavv@gmail.com>
2023-07-20 17:28:35 +0500

    form styles done

--- client/src/pages/Cleaner/LandingCleaner.tsx
+++ client/src/pages/Cleaner/LandingCleaner.tsx
@@ -114,74 +114,7 @@ export default function LandingCleaner() {
           <div className={styles.cleanerRegistrationDiv} id="cleanerRegForm">
             <CleanerRegister />
           </div>
-
           <CleanerFooter />
-
-          {/* <section className="footer">
-            <div className="first-box">
-              <div className="link-title">Меню</div>
-              <a className="link-footer" href="/">
-                Главная
-              </a>
-              <a className="link-footer" href="#plus">
-                О компании
-              </a>
-
-              <Link className="link-footer" to="/jobs">
-                Вакансии
-              </Link>
-
-              <a className="link-footer" href="#price">
-                Цены
-              </a>
-              <a className="link-footer" href="*">
-                Договор
-              </a>
-              <a className="link-footer" href="*">
-                Политика конфиденциальности
-              </a>
-              <a className="link-footer" href="*">
-                Правила работы сайта
-              </a>
-            </div>
-
-            <div className="contact-box">
-              <div className="link-title">Контакты</div>
-              <a className="link-footer" href="tel:8 800-2222-945">
-                8 800-2222-945
-              </a>
-              <a className="link-footer" href="tel:8 (921) 928-72-58">
-                8 (921) 928-72-58
-              </a>
-              <a className="link-footer" href="mailto:info@clean-master.com">
-                info@clean-master.com
-              </a>
-
-              <div className="social">
-                <a href="https://vk.com/club134865736">
-                  <img className="soc-png" src="./s2.png" alt="" />
-                </a>
-                <a href="https://www.instagram.com/cleanmastergt/">
-                  <img className="soc-png" src="./s1.png" alt="" />
-                </a>
-                <a href="https://web.telegram.org/a/#-1751514282">
-                  <img className="soc-png" src="./s4.png" alt="" />
-                </a>
-                <a className="link-footer" href="https://web.whatsapp.com/">
-                  <img className="soc-png" src="./s5.png" alt="" />
-                </a>
-              </div>
-            </div>
-
-            <div className="three-box">
-              <div className="link-title">Способы оплаты</div>
-              <div className="pays-box">
-                <img className="pay1" src="./p1.png" alt="" />
-                <img className="pay" src="./p2.png" alt="" />
-                <img className="pay" src="./p3.png" alt="" />
-              </div>
-            </div>
-          </section> */}
         </Space>
       </ConfigProvider>
     </>
