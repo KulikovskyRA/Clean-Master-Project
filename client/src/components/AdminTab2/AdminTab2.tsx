9f6d2a36a0c8611bd6e40b2bef0daf941b02347c
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-07-21 13:32:58 +0300

    fix0

--- client/src/components/AdminTab2/AdminTab2.tsx
+++ client/src/components/AdminTab2/AdminTab2.tsx
@@ -245,7 +245,7 @@ const AdminTab2 = () => {
         <Form
           name="basic"
           labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
+          // wrapperCol={{ span: 16 }}
           style={{ maxWidth: 600 }}
           initialValues={{ remember: true }}
           onFinish={onFinish}
@@ -283,7 +283,7 @@ const AdminTab2 = () => {
 
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Button type="primary" htmlType="submit">
-              Submit
+              Создать услугу
             </Button>
           </Form.Item>
         </Form>
